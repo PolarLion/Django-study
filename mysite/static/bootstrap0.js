df30cf26d6e6b69cbf74a1063523b5a39d31fc6b
PolarLion <545135082@qq.com>
2015-09-20 19:46:43 +0800

    add many

--- mysite/static/bootstrap0.js
+++ mysite/static/bootstrap0.js
@@ -1,5 +1,5 @@
 $(function() {
-	$("#translate").on("click", function() {
+	$("#nmt").on("click", function() {
 		// debugger;
 /*		if ("disabled" == $(this).attr("disabled")) {
 			return;
@@ -8,6 +8,23 @@ $(function() {
 		var var1=document.getElementById("sourceinput").value;
 		$("#targetinput").html(decodeURI(var1));
 	});
-
+	$("#smt").on("click", function() {
+		// debugger;
+/*		if ("disabled" == $(this).attr("disabled")) {
+			return;
+		}
+		$(this).attr("disabled", "disabled");*/
+		var var1=document.getElementById("sourceinput").value;
+		$("#targetinput").html(decodeURI(var1));
+	});
+	$("#save-result").on("click", function() {
+		// debugger;
+/*		if ("disabled" == $(this).attr("disabled")) {
+			return;
+		}
+		$(this).attr("disabled", "disabled");*/
+		// var var1=document.getElementById("sourceinput").value;
+		$("#targetinput").html(decodeURI("sssssss"));
+	});
 });
 
