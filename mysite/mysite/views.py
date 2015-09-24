84a70deb3c7f81328241e591ed3a4c7347c2055f
PolarLion <545135082@qq.com>
2015-09-24 15:07:32 +0800

    heihei

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import datetime
+import sys
+# sys.path.remove("/home/xwshi/anaconda/pkgs/")
+if not "/home/xwshi/project/py/GroundHog/experiments/nmt/" in sys.path:
+  sys.path.append("/home/xwshi/project/py/GroundHog/experiments/nmt/")
+if not "/home/xwshi/project/py/GroundHog/" in sys.path:
+  sys.path.append("/home/xwshi/project/py/GroundHog/")
+if not "/home/xwshi/anaconda/lib/python2.7/site-packages/" in sys.path:
+  sys.path.append("/home/xwshi/anaconda/lib/python2.7/site-packages/")
+# import translateserver
 
 def current_datetime(request):
   now = datetime.datetime.now()
@@ -13,6 +22,30 @@ def test_bootstrap(request):
   #return HttpResponse('starter-template/index.html')
   return render_to_response('starter-template/index.html', {})
 
+def nmt(request, a):
+	words = a.encode('utf-8').decode("utf-8")
+	state = 0
+	new_line = ""
+	for word in words:
+		ch = word.encode('utf-8')
+		if (ch.isalnum() or ch == '.') and state == 0:
+			new_line += " " + ch
+			state = 1
+		elif state == 1 and (ch.isalnum() or ch == '.'):
+			new_line += ch
+		elif ch.isspace():
+			a = 0
+		elif not ch.isalnum():
+			state = 0
+			new_line += " " + ch
+	new_line = new_line.strip()
+	# return  HttpResponse(s)
+	# return  HttpResponse(translateserver.translate(s))
+  	# re =  translateserver.translate(ss)
+  	# return  HttpResponse(new_line)
+  	return  HttpResponse(translateserver.translate(new_line))
+
+
 def bootstrap0(request):
-  return render_to_response('bootstrap0.html', {'STATIC_URL': '/static/'})
+  return render_to_response('index.html', {'STATIC_URL': '/static/'})
   #return render_to_response('starter-template/index.html', {})
--- mysite/static/bootstrap0.js
+++ mysite/static/bootstrap0.js
@@ -5,8 +5,27 @@ $(function() {
 			return;
 		}
 		$(this).attr("disabled", "disabled");*/
+		var xmlhttp;
+		if (window.XMLHttpRequest) {
+		    // code for IE7+, Firefox, Chrome, Opera, Safari
+		    xmlhttp=new XMLHttpRequest();
+		  } else {
+		    // code for IE6, IE5
+		    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
 		var var1=document.getElementById("sourceinput").value;
-		$("#targetinput").html(decodeURI(var1));
+		xmlhttp.open("GET", "/nmt/" + var1, true);
+		xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		xmlhttp.send();
+        // $.ajax("/nmt/",{'a':var1}, function(ret){
+        //     $('#targetinput').html(decodeURI(ret))
+        // })
+		xmlhttp.onreadystatechange=function() {
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+		  	// $('#targetinput').html(decodeURI(ret))
+		      document.getElementById("targetinput").innerHTML=xmlhttp.responseText;
+		  }
+		}
 	});
 	$("#smt").on("click", function() {
 		// debugger;
